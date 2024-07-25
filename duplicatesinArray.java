7bb77917ae3fa866d2b1860fd67fa0e105b5ca22
MOUBANI SHEE <165894661+moubani2002@users.noreply.github.com>
2024-07-25 23:38:10 +0530

    day 5

---
+++ duplicatesinArray.java
@@ -0,0 +1,16 @@
+//duplicates in java
+
+class Solution {
+    public List<Integer> findDuplicates(int[] nums) {
+        List result = new ArrayList<>();
+        for (int n : nums) {
+            n = Math.abs(n);
+            if (nums[n - 1] > 0) {
+                nums[n - 1] *= -1;
+            } else {
+                result.add(n);
+            }
+        }
+        return result;
+    }
+}
