1ae670e5c7270a6ba12de8c92cedd30f92136a0f
MOUBANI SHEE <165894661+moubani2002@users.noreply.github.com>
2024-07-27 23:43:11 +0530

    day 7

---
+++ maxPointsFromCards.java
@@ -0,0 +1,21 @@
+//Maximum points one can obtain from cards
+
+class Solution {
+    public int maxScore(int[] cardPoints, int k) {
+        int len = cardPoints.length;
+        int st = len - k;
+        int end = st;
+        int sum = 0;
+        int res = 0;
+        for(int i = 0; i < k * 2; i++){
+            sum += cardPoints[end % len];
+            if(i >= k - 1){
+                res = Math.max(res, sum);
+                sum -= cardPoints[st % len];
+                st++;
+            }
+            end++;
+        }
+        return res;
+    }
+}
