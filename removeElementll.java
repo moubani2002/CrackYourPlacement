73d04a67e091a9ecdcd4cd90a2fca9d2dac5f00c
MOUBANI SHEE <165894661+moubani2002@users.noreply.github.com>
2024-08-07 23:46:02 +0530

    day 18

---
+++ removeElementll.java
@@ -0,0 +1,29 @@
+//Remove element from a linked list
+
+class Solution {
+    public ListNode removeElements(ListNode head, int val) {
+        if (head == null) {
+            return head;
+        }
+
+        // Let's do this by the dummy process
+        ListNode dummy = new ListNode(0);
+        ListNode current = dummy;
+
+        ListNode temp = head;
+
+        while (temp != null) {
+            if (temp.val != val) {
+                current.next = temp;
+                temp = temp.next;
+                current = current.next;
+            } else {
+                temp = temp.next;
+            }
+        }
+
+        current.next = null;
+
+        return dummy.next;
+    }
+}
